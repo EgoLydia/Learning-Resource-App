0307cb5e5c33cd124c0e1b89c0576ca016fbb443
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-08-04 14:19:46 +0100

    create comp collecting data from root app via prop

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="data in resources"
+      :key="data.id"
+      :title="data.title"
+      :description="data.description"
+      :link="data.link"
+      >{{ data }}</learning-resource
+    >
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  props: ['resources'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
