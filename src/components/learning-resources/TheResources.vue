037f2a1f868f518318156effe6d47f26a22286e0
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-08-04 15:26:32 +0100

    set button modes using inject and provide

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,66 @@
+<template>
+  <base-card>
+    <base-button
+      :mode="storedResButtonMode"
+      @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button
+      :mode="addResButtonMode"
+      @click="setSelectedTab('add-resource')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google Official Guide',
+          description: 'The official Google documentation',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
+
+<style></style>
